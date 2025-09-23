f8c7e73db879d1f2ce3fae5751e1d2558935b33c
d_tolkunov <d_tolkunov@gh.uz>
2025-09-23 12:50:51 +0500

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import './i18n'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  /* </React.StrictMode> */
+  ,
 )
